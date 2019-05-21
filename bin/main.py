9ce2ffb4997ca9aee1665ce86c9a4d12594af12f
alu0100813272 <alu0100813272@ull.etsi.es>
2019-05-21 02:28:18 +0100

    Reorganizar archivos

--- bin/main.py
+++ bin/main.py
@@ -1,57 +1,71 @@
-#!/usr/bin/python
+# !/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Creado por: Jairo Gonzalez Lemus alu0100813272@ull.edu.es
 # Ejemplo: cojo archivo
-from face import face
+#from face import face
 import subprocess
 import sys
 import threading
 import cv2
+import os
 
-print("Entreeeeeee")
-recognizer = face.Face()
+pathDir = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+pid = str(os.getpid())
+pidfile = pathDir + "/tmp/mydaemon.pid"
+
+try:
+    if os.path.isfile(pidfile):
+        print("%s el archivo ya existe, cerrando el proceso" % pidfile)
+        raise ValueError('The program is in process')
+    else:
+        open(pidfile, "w").write(pid)
+        while True:
+            print("ejecuto program")
+
+except Exception as e:
+    print('Exception message: ' + str(e))
+finally:
+    os.unlink(pidfile)
 
 
-pathAntes = os.path.dirname(sys.argv[0])
-path = pathAntes  + '/fingers.py'
 
-def __main__():
-    recognizer.create_fisher_recognizer()
-    cont_name = 0
-    name_aux = ""
-    while True:
-        if(opcion == "1"):
-            print("opcion dentro")
-            recognizer.new_capture("opcion")
-        else:
-            # print("opcion" + opcion)
-            (reco, name, img) = recognizer.recognizer()
-            if(name_aux == name) & (name != "Desconocido"):
-                cont_name += 1
-                if(cont_name >= 10):
-                    cont_name = -10
-                    # num_1 = 100
-                    # datos = ['sudo', 'python', 'fingers.py', name]
-                    datos = ['python', path, path, name]
-                    # datos = ['./fingers.py', name]
-                    p = subprocess.Popen(datos)
+
+# # showing stat information of file "foo.txt"
+# statinfo = os.stat('foo.txt')
+# pathDir = os.path.abspath(os.path.realpath(__file__))
+# pathAntes = os.path.dirname(sys.argv[0])
+# path = pathDir  + '/fingers.py'
+
+# def __main__():
+#     recognizer.create_fisher_recognizer()
+#     cont_name = 0
+#     name_aux = ""
+#     while True:
+#         if(opcion == "1"):
+#             print("opcion dentro")
+#             recognizer.new_capture("opcion")
+#         else:
+#             # print("opcion" + opcion)
+#             (reco, name, img) = recognizer.recognizer()
+#             if(name_aux == name) & (name != "Desconocido"):
+#                 cont_name += 1
+#                 if(cont_name >= 10):
+#                     cont_name = -10
+#                     # num_1 = 100
+#                     # datos = ['sudo', 'python', 'fingers.py', name]
+#                     datos = ['python', path, path, name]
+#                     # datos = ['./fingers.py', name]
+#                     p = subprocess.Popen(datos)
                    
                   
-                    # stdout, stderr = p.communicate()
-                    # print p
-                    # print name
-            else:
-                name_aux = name
-                cont_name = 0
-            # mira para gaursa
-            # recognizer.saveVideo("./face/",img)
-            cv2.imshow("fas",img)
-            recognizer.save_video(img)
-         
-
-
-
-__main__()
-
-
+#                     # stdout, stderr = p.communicate()
+#                     # print p
+#                     # print name
+#             else:
+#                 name_aux = name
+#                 cont_name = 0
+#             # mira para gaursa
+#             # recognizer.saveVideo("./face/",img)
+#             cv2.imshow("fas",img)
+#             recognizer.save_video(img)
 
